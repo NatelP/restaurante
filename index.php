06bc8049afd3a74810d7fa77c28aebf4df03aa46
NatelP <122240240+NatelP@users.noreply.github.com>
2023-06-01 17:56:03 -0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conexion = oci_connect("system" , "semestre3" , "localhost/xe");
-    if(!$conexion){
-        $m=oci_error();
-        echo $m['message'],"n";
-        exit;
-    }else{
-        echo "Conexion Con Exito a oracle";}
-?>
